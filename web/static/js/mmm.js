3807c148df97a206e7bb52a242928c240fcc258b
Peter Turczak <peter@turczak.de>
2020-04-21 10:18:43 +0200

    Add play buttons in overview

--- web/static/js/mmm.js
+++ web/static/js/mmm.js
@@ -1,7 +1,26 @@
+
 $(function() {
+	// CSRF stuff taken from flask-wtf.readthedocs.io
+	let csrf_token = $("[data-csrf]").data('csrf');
+	$.ajaxSetup({
+		beforeSend: function(xhr, settings) {
+			if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+				xhr.setRequestHeader("X-CSRFToken", csrf_token);
+			}
+		}
+	});
+
+	$('div.play').click(function(event){
+		let data = $(event.target).data();
+		$.post('/music/control/play', data, function(){
+			console.log("Update seems to have worked..");
+		});
+
+		console.log();
+	});
 	$('form[data-usage="assign_tag"]').submit(function(event) {
 		let form = event.target;
-		
+
 		let data = {"AJAX": true};
 		$("input", form).each(function(){
 			data[this.name] = this.value;
@@ -21,4 +40,4 @@ $(function() {
 		*/
 		return false;
 	});
-	});
+});
